e86f98df84623cfc8cda8a51edb7adbdc7f67f05
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-05-23 19:54:26 +0300

    Fixed challenge 1 for python3.

--- set_1/challenge_1.py
+++ set_1/challenge_1.py
@@ -5,5 +5,5 @@ hex_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69
 
 raw_bytes = bytearray.fromhex(hex_string)
 
-print(base64.b64encode(raw_bytes) == 
+print(base64.b64encode(raw_bytes).decode() ==
       'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t')
