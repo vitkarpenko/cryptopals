d50fd2f72bc8368b0d4c77b397bdad780b77c8bc
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-05-23 23:07:34 +0300

    1/3: WIP.

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -2,6 +2,7 @@
 """
 
 from collections import Counter
+import operator
 import os
 import string
 
@@ -10,11 +11,11 @@ ENCODED_STRING = ('1b37373331363f78151b7f2b783431333d'
                   '78397828372d363c78373e783a393b3736')
 
 
-def calculate_frequency_difference(counter1, counter2):
+def calculate_frequency_likeness(counter1, counter2):
     """Finds how many most frequent keys match.
     """
     return sum(1 for c1, c2 in zip(counter1.most_common(),
-                                   counter2.most_common()) if c1 == c2)
+                                   counter2.most_common()) if c1[0] == c2[0])
 
 
 def generate_decrypted_string(cipher_key):
@@ -37,10 +38,11 @@ cipher_key_ranking = dict()
 for cipher_key in range(256):
     decrypted_string = generate_decrypted_string(cipher_key)
     decrypted_string_counter = Counter(decrypted_string)
-    cipher_key_ranking[cipher_key] = calculate_frequency_difference(
+    cipher_key_ranking[cipher_key] = calculate_frequency_likeness(
         book_counter, decrypted_string_counter
     )
 
-minimum_frequency_difference = min(cipher_key_ranking,
-                                   key=cipher_key_ranking.get)
-best_cipher_key = cipher_key_ranking[minimum_frequency_difference]
+best_cipher_key = max(cipher_key_ranking.items(),
+                      key=operator.itemgetter(1))[0]
+
+print(generate_decrypted_string(best_cipher_key))
