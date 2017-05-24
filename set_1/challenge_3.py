8170e10e489dafc33723f9ff9e6adea12ed4d0db
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-05-24 08:28:43 +0300

    1/3: changed scoring method. 1/3: Done!

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -2,20 +2,11 @@
 """
 
 from collections import Counter
-import operator
 import os
-import string
 
 
 ENCODED_STRING = ('1b37373331363f78151b7f2b783431333d'
                   '78397828372d363c78373e783a393b3736')
-
-
-def calculate_frequency_likeness(counter1, counter2):
-    """Finds how many most frequent keys match.
-    """
-    return sum(1 for c1, c2 in zip(counter1.most_common(),
-                                   counter2.most_common()) if c1[0] == c2[0])
 
 
 def generate_decrypted_string(cipher_key):
@@ -31,16 +22,23 @@ with open(os.path.join('data', 'book.txt')) as book:
     for line in book:
         book_counter.update(
             character.lower() for character in line
-            if character in string.ascii_letters
         )
-print(book_counter.most_common(10))
+
+most_frequent_book_letters = set(
+    entry[0]
+    for entry in book_counter.most_common(10)
+)
 
 cipher_key_ranking = dict()
 for cipher_key in range(256):
     decrypted_string = generate_decrypted_string(cipher_key)
     decrypted_string_counter = Counter(decrypted_string)
-    cipher_key_ranking[cipher_key] = calculate_frequency_likeness(
-        book_counter, decrypted_string_counter
+    most_frequent_string_letters = set(
+        entry[0]
+        for entry in decrypted_string_counter.most_common(10)
+    )
+    cipher_key_ranking[cipher_key] = len(
+        most_frequent_book_letters & most_frequent_string_letters
     )
 
 maximum_frequency_likeness = max(cipher_key_ranking.values())
