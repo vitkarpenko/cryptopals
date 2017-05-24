e5fddb071a7ff535483195843b3fc71441c75263
Vitaly Karpenko <vitkarpenko@gmail.com>
2017-05-24 09:14:10 +0300

    1/3: restructure. 1/4: Done!

--- set_1/challenge_3.py
+++ set_1/challenge_3.py
@@ -9,45 +9,62 @@ ENCODED_STRING = ('1b37373331363f78151b7f2b783431333d'
                   '78397828372d363c78373e783a393b3736')
 
 
-def generate_decrypted_string(cipher_key):
+def generate_decrypted_string(cipher_key, string):
     try:
         return bytes(b1 ^ cipher_key
-                     for b1 in bytes.fromhex(ENCODED_STRING)).decode().lower()
-    except UnicodeDecodeError:
+                     for b1 in bytes.fromhex(string)).decode()
+    except UnicodeDecodeError as err:
         pass
+    except ValueError as err:
+        print(err)
 
 
-book_counter = Counter()
-with open(os.path.join('data', 'book.txt')) as book:
-    for line in book:
-        book_counter.update(
-            character.lower() for character in line
+def find_most_frequent_letters_in_txt(path):
+    txt_counter = Counter()
+    with open(path) as txt:
+        for line in txt:
+            txt_counter.update(
+                character for character in line
+            )
+    return set(entry[0] for entry in txt_counter.most_common(10))
+
+
+def find_best_cipher_keys(frequent_letters, string):
+    cipher_key_ranking = dict()
+    for cipher_key in range(256):
+        decrypted_string = generate_decrypted_string(cipher_key,
+                                                     string)
+        decrypted_string_counter = Counter(decrypted_string)
+        most_frequent_string_letters = set(
+            entry[0]
+            for entry in decrypted_string_counter.most_common(10)
+        )
+        cipher_key_ranking[cipher_key] = len(
+            frequent_letters & most_frequent_string_letters
         )
 
-most_frequent_book_letters = set(
-    entry[0]
-    for entry in book_counter.most_common(10)
-)
+    maximum_frequency_likeness = max(cipher_key_ranking.values())
+    if maximum_frequency_likeness == 0:
+        return []
 
-cipher_key_ranking = dict()
-for cipher_key in range(256):
-    decrypted_string = generate_decrypted_string(cipher_key)
-    decrypted_string_counter = Counter(decrypted_string)
-    most_frequent_string_letters = set(
-        entry[0]
-        for entry in decrypted_string_counter.most_common(10)
+    return [
+        key
+        for key, value in cipher_key_ranking.items()
+        if value == maximum_frequency_likeness
+    ]
+
+
+def main():
+    most_frequent_book_letters = find_most_frequent_letters_in_txt(
+        os.path.join('data', 'book.txt')
     )
-    cipher_key_ranking[cipher_key] = len(
-        most_frequent_book_letters & most_frequent_string_letters
-    )
 
-maximum_frequency_likeness = max(cipher_key_ranking.values())
+    best_cipher_keys = find_best_cipher_keys(most_frequent_book_letters,
+                                             ENCODED_STRING)
 
-best_cipher_keys = [
-    key
-    for key, value in cipher_key_ranking.items()
-    if value == maximum_frequency_likeness
-]
+    for key in best_cipher_keys:
+        print(generate_decrypted_string(key, ENCODED_STRING))
 
-for key in best_cipher_keys:
-    print(generate_decrypted_string(key))
+
+if __name__ == '__main__':
+    main()
---
+++ set_1/challenge_4.py
@@ -0,0 +1,29 @@
+"""Detect single-character XOR.
+"""
+
+import os
+import challenge_3
+
+
+def decrypt_all_strings(path):
+    most_frequent_book_letters = challenge_3.find_most_frequent_letters_in_txt(
+        os.path.join('data', 'book.txt')
+    )
+
+    with open(path) as data:
+        for line in data:
+            line = line.rstrip('\n')
+            best_cipher_keys = challenge_3.find_best_cipher_keys(
+                most_frequent_book_letters,
+                line
+            )
+            for key in best_cipher_keys:
+                print(challenge_3.generate_decrypted_string(key, line))
+
+
+def main():
+    decrypt_all_strings(os.path.join('data', 'challenge_4.txt'))
+
+
+if __name__ == '__main__':
+    main()
